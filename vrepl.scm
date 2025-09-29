626c05efe42bf718deb499e4c6a49758affd03de
Tsugu <146515030+tsugus@users.noreply.github.com>
2025-09-30 01:25:49 +0900

    fix
    Changes to be committed:
    	modified:   vrepl.scm

--- vrepl.scm
+++ vrepl.scm
@@ -58,11 +58,10 @@
   (repl-body< an_eval *top-exp*)
   (repl< an_eval))
 ;
-(define repl<
-  (lambda args
-    (if (null? args) (set! args '(eval<)))  ; the defalut eval function is 'eval<'.
-    (set! *top-exp* (read))
-    (cond
-      ((atom? *top-exp*) (repl-loop< (car args)))
-      ((eq? 'exit (car *top-exp*)) (display ""))
-      (else (repl-loop< (car args))))))
+(define (repl< . args)
+  (if (null? args) (set! args '(eval<)))  ; the defalut eval function is 'eval<'.
+  (set! *top-exp* (read))
+  (cond
+    ((atom? *top-exp*) (repl-loop< (car args)))
+    ((eq? 'exit (car *top-exp*)) (display ""))
+    (else (repl-loop< (car args)))))
